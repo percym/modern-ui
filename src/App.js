b296c8a99b7f7b6581639b1ecfc303ce54374a91
percy mugadza <percy.mugadza@ecocashholdings.co.zw>
2022-09-08 14:05:39 +0200

    fixed ReactDom import

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Article from './components/article/Article';
+import Brand from './components/brand/Brand';
+import CTA  from './components/cta/CTA';
+import Feature  from './components/feature/Feature';
+import NavBar from './components/navbar/NavBar';
+
+
 
 const  App = ()=> {
   return (
     <div>
         <h1>GPT-3</h1>
+        <Article />
     </div>
   )
 }
