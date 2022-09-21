031cbde03bddca9f68c299592309146c74c8829f
percy mugadza <percy.mugadza@ecocashholdings.co.zw>
2022-09-21 12:36:58 +0200

    going what3gpt

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -15,11 +15,11 @@ const Header = () => {
           <input type="email" placeholder='Your email address' />
           <button type='button'>Get Started</button>
         </div>
-      
-      <div className='gpt3__header-content__people'>
-        <img src={people} alt='people' />
-        <p>1,600 people requested access a visit in last 24 hours</p>
-      </div>
+
+        <div className='gpt3__header-content__people'>
+          <img src={people} alt='people' />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
       </div>
       <div className='gpt3__header-image'>
         <img src={ai} />
